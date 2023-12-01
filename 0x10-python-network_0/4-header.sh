d1de92806ecddd9065430d5646fdd0d53ae39a3a
dracula0007 <make.mony.as7@gmail.com>
2023-12-01 10:15:52 -0800

    cURL headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL as an argument
-curl "$1" -sX GET -H "X-HolbertonSchool-User-Id:98"
+# This script takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response. Also, a header variable X-School-User-Id must be sent with the value 98.
+curl -s "$1" -X GET -H "X-School-User-Id: 98"
